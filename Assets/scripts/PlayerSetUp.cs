d2758d7bcb632637cfcef83dd0abe8a088a4c720
Matt Key <matthew.key@outlook.com>
2019-10-03 22:00:49 +0100

    Basic Unet networking

---
+++ Assets/scripts/PlayerSetUp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetUp : NetworkBehaviour
+{
+    [SerializeField]
+    public Component[] Components;
+
+    public Camera cam;
+    private GameObject mainCam;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            foreach (var c in Components)
+                Destroy(c);
+            Destroy(cam);
+            return;
+        }
+        mainCam = Camera.main.gameObject;
+
+        if (mainCam != null)
+            mainCam.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        if (mainCam != null)
+            mainCam.SetActive(true);
+    }
+
+}
